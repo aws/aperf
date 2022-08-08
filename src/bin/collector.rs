23f09130914ff292f16cdcc8305d63811045d33b
Janakarajan Natarajan <janakan@amazon.com>
2022-08-08 23:04:02 +0000

    Add support for collecting CPU Utilization data
    Use procfs and collect CPU utilization data (both aggregate and per cpu)
    from /proc/stat.
    
    The data collected is stored in a YAML file.

---
+++ src/bin/collector.rs
@@ -0,0 +1,47 @@
+#[macro_use]
+extern crate log;
+use env_logger::Env;
+
+use clap::Parser;
+use performance_data::PERFORMANCE_DATA;
+use performance_data::{InitParams, PDResult};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Interval (in seconds) at which performance data is to be collected.
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    interval: u64,
+
+    /// Time (in seconds) for which the performance data is to be collected.
+    #[clap(short, long, value_parser, default_value_t = 10)]
+    period: u64,
+}
+
+fn init_logger() {
+    let env = Env::default().filter("PDA_LOG_LEVEL");
+
+    env_logger::init_from_env(env);
+}
+
+fn start_collection_serial() -> PDResult {
+    info!("Collecting data serially...");
+    PERFORMANCE_DATA.lock().unwrap().collect_data_serial()?;
+    Ok(())
+}
+
+fn main() -> PDResult {
+    let args = Args::parse();
+    let mut params = InitParams::new();
+
+    /* Initialize logging system */
+    init_logger();
+
+    params.period = args.period;
+    params.interval = args.interval;
+    PERFORMANCE_DATA.lock().unwrap().set_params(params);
+    PERFORMANCE_DATA.lock().unwrap().init_collectors()?;
+    info!("Starting Performance Data collection:");
+    start_collection_serial()?;
+    Ok(())
+}
--- src/data.rs
+++ src/data.rs
@@ -1,4 +1,7 @@
+pub mod cpu_utilization;
+
 use crate::{InitParams, PDResult};
+use cpu_utilization::CpuUtilization;
 use log::debug;
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
@@ -47,19 +50,107 @@ impl DataType {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Data {
-    None,
+/// Create a Data Enum
+///
+/// Each enum type will have a collect_data implemented for it.
+macro_rules! data {
+    ( $( $x:ident ),* ) => {
+        #[derive(Serialize, Deserialize, Debug)]
+        pub enum Data {
+            $(
+                $x($x),
+            )*
+        }
+
+        impl Data {
+            fn collect_data(&mut self) -> PDResult {
+                match self {
+                    $(
+                        Data::$x(ref mut value) => value.collect_data()?,
+                    )*
+                }
+                Ok(())
+            }
+        }
+    };
 }
 
-impl Data {
-    fn collect_data(&mut self) -> PDResult {
-        match self {
-            Data::None => Ok(()),
-        }
-    }
-}
+data!(CpuUtilization);
 
 pub trait CollectData {
     fn collect_data(&mut self) -> PDResult;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::cpu_utilization::CpuUtilization;
+    use super::Data;
+    use super::DataType;
+    use crate::InitParams;
+    use serde::Deserialize;
+    use serde_yaml::{self};
+    use std::fs;
+    use std::path::Path;
+
+    #[test]
+    fn test_data_type_init() {
+        let mut param = InitParams::new();
+        let data = CpuUtilization::new();
+        let mut dt = DataType {
+            data: Data::CpuUtilization(data),
+            file_handle: None,
+            file_name: "cpu_utilization".to_string(),
+            full_path: String::new(),
+            dir_name: String::new(),
+        };
+
+        param.dir_name = format!("./performance_data_init_test_{}", param.time_str);
+        let _ret = fs::DirBuilder::new()
+            .recursive(true)
+            .create(param.dir_name.clone())
+            .unwrap();
+
+        dt.init_data_type(param).unwrap();
+
+        assert!(!dt.file_handle.is_none());
+        fs::remove_file(dt.full_path).unwrap();
+        fs::remove_dir_all(dt.dir_name).unwrap();
+    }
+
+    #[test]
+    fn test_print() {
+        let mut param = InitParams::new();
+        let data = CpuUtilization::new();
+        let mut dt = DataType {
+            data: Data::CpuUtilization(data),
+            file_handle: None,
+            file_name: "cpu_utilization".to_string(),
+            full_path: String::new(),
+            dir_name: String::new(),
+        };
+
+        param.dir_name = format!("./performance_data_print_test_{}", param.time_str);
+        let _ret = fs::DirBuilder::new()
+            .recursive(true)
+            .create(param.dir_name.clone())
+            .unwrap();
+
+        dt.init_data_type(param).unwrap();
+
+        assert!(Path::new(&dt.full_path).exists());
+        assert!(dt.print_to_file().unwrap() == ());
+
+        for document in serde_yaml::Deserializer::from_reader(dt.file_handle.unwrap()) {
+            let v = Data::deserialize(document).expect("File read error");
+            match v {
+                Data::CpuUtilization(ref value) => {
+                    assert!(value.total.cpu == 0);
+                    assert!(value.per_cpu.is_empty());
+                }
+                _ => assert!(true),
+            }
+        }
+        fs::remove_file(dt.full_path).unwrap();
+        fs::remove_dir_all(dt.dir_name).unwrap();
+    }
 }
