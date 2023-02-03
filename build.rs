b94d37362f662eab29d97bd77c3745a119f12294
William Ashley <wash@amazon.com>
2023-02-03 15:44:47 +0000

    build dependencies: stop depending on anything other than 'npm' being installed, and version control our npm-sourced dependencies

--- build.rs
+++ build.rs
@@ -3,8 +3,21 @@ use std::process::Command;
 use vergen::{Config, ShaKind, vergen};
 
 fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=package.json");
+    println!("cargo:rerun-if-changed=package-lock.json");
+    let status = Command::new("npm")
+        .arg("install")
+        .spawn()?
+        .wait()?;
+    if ! status.success() {
+        std::process::exit(1);
+    }
+
     println!("cargo:rerun-if-changed=src/bin/html_files/");
-    let status = Command::new("tsc")
+    let status = Command::new("npm")
+        .arg("exec")
+        .arg("--")
+        .arg("tsc")
         .arg("-b")
         .arg("src/bin/html_files/")
         .arg("--verbose")
@@ -13,6 +26,7 @@ fn main() -> Result<()> {
     if ! status.success() {
         std::process::exit(1);
     }
+
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     vergen(config)
