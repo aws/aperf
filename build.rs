9f6740bbfff3033187d15ab2bf58cefb532b42ed
William Ashley <wash@amazon.com>
2023-02-03 15:29:31 +0000

    build.rs: don't ignore failures invoking the typescript compiler, and add its (to be relocated) output directory to .gitignore

--- build.rs
+++ build.rs
@@ -4,12 +4,15 @@ use vergen::{Config, ShaKind, vergen};
 
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=src/bin/html_files/");
-    Command::new("tsc")
+    let status = Command::new("tsc")
         .arg("-b")
         .arg("src/bin/html_files/")
         .arg("--verbose")
         .spawn()?
         .wait()?;
+    if ! status.success() {
+        std::process::exit(1);
+    }
     let mut config = Config::default();
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     vergen(config)
