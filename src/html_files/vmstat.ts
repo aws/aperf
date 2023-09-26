67c6cb227644a396fc297611638bd0f648752d00
Janakarajan Natarajan <janakan@amazon.com>
2023-09-26 16:35:05 +0000

    Add EmptyGraph support for Vmstat

--- src/html_files/vmstat.ts
+++ src/html_files/vmstat.ts
@@ -1,19 +1,17 @@
 let got_vmstat_data = false;
 
 function getEntries(run, container_id, keys, run_data) {
-    var data = keys;
-    data.forEach(function (value, index, arr) {
+    for (let i = 0; i < all_run_keys.length; i++) {
+        let value = all_run_keys[i];
         var elem = document.createElement('div');
         elem.id = `vmstat-${run}-${value}`;
         elem.style.float = "none";
         addElemToNode(container_id, elem);
-        setTimeout(() => {
-            getEntry(run, elem.id, value, run_data[value]);
-        }, 0);
-    })
+        emptyOrCallback(keys, getEntry, elem, value, run_data);
+    }
 }
 
-function getEntry(run, parent_id, key, run_data) {
+function getEntry(elem, key, run_data) {
     var data = JSON.parse(run_data);
     var x_time = [];
     var y_data = [];
@@ -21,9 +19,6 @@ function getEntry(run, parent_id, key, run_data) {
         x_time.push(value.time.TimeDiff);
         y_data.push(value.value);
     });
-    var elem = document.createElement('div');
-    elem.style.float = "none";
-    addElemToNode(parent_id, elem);
     var TESTER = elem;
     var vmstat_data: Partial<Plotly.PlotData> = {
         x: x_time,
