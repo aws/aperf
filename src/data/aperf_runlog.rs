bf8437e913b1f84b8997e4369b9f57020163f4ca
lancelui-amzn <lancelui@amazon.com>
2025-10-29 13:22:48 -0500

    Process all text data into uniform format

--- src/data/aperf_runlog.rs
+++ src/data/aperf_runlog.rs
@@ -1,4 +1,5 @@
-use crate::data::ProcessedData;
+use crate::data::data_formats::{AperfData, TextData};
+use crate::data::{Data, ProcessedData};
 use crate::utils::DataMetrics;
 use crate::visualizer::{GetData, ReportParams};
 use anyhow::Result;
@@ -39,6 +40,30 @@ impl GetData for AperfRunlog {
         }
         raw_data.push(ProcessedData::AperfRunlog(runlog));
         Ok(raw_data)
+    }
+
+    fn process_raw_data_new(
+        &mut self,
+        params: ReportParams,
+        _raw_data: Vec<Data>,
+    ) -> Result<AperfData> {
+        let mut text_data = TextData::default();
+
+        let file = fs::OpenOptions::new()
+            .read(true)
+            .open(params.data_file_path)
+            .expect("Could not open Aperf Runlog file");
+        let mut reader = BufReader::new(file);
+        let mut line = String::new();
+        while reader.read_line(&mut line)? > 0 {
+            if line.ends_with('\n') {
+                line = line.trim_end().to_string();
+            }
+            text_data.lines.push(line.clone());
+            line.clear();
+        }
+
+        Ok(AperfData::Text(text_data))
     }
 
     fn get_calls(&mut self) -> Result<Vec<String>> {
